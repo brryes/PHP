08c8264ec61e1396eaec28341cddc121cf9732fa
DarkBolt_24 <karlgabriel@gmail.com>
2025-07-12 14:17:30 +0800

    Update home.php

--- home.php
+++ home.php
@@ -10,17 +10,17 @@ $hour = date('H');
 
 // Determine greeting and corresponding Tailwind color class
 if ($hour < 12) {
-  $greeting = "Good morning";
+  $greeting = "Hello User";
   $bgColor = "bg-yellow-500/10";
   $borderColor = "border-yellow-500";
   $textColor = "text-yellow-400";
 } elseif ($hour < 18) {
-  $greeting = "Good afternoon";
+  $greeting = "Hello User";
   $bgColor = "bg-orange-500/10";
   $borderColor = "border-orange-500";
   $textColor = "text-orange-400";
 } else {
-  $greeting = "Good evening";
+  $greeting = "Hello User";
   $bgColor = "bg-indigo-600/10";
   $borderColor = "border-indigo-500";
   $textColor = "text-indigo-400";
